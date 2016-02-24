9337b5680ba8faf20d5a5929062db1e263caf614
goalong <goforalong@gmail.com>
2016-02-24 11:05:30 +0800

    create project tutorial

---
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,12 @@
+import scrapy 
+
+class DmozSpider(scrapy.Spider):
+	name = 'dmoz'
+	start_urls = [
+		"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+			]
+	def parse(self, response):
+		filename = response.url.split('/')[-2] + '.html'
+		with open(filename, 'wb') as f:
+			f.write(response.body)
