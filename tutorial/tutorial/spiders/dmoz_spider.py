9655fc737712f9ffb1240c2c19ac095c373067d1
goalong <goforalong@gmail.com>
2016-02-25 15:32:09 +0800

    modify dmoz spider

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,4 +1,6 @@
 import scrapy 
+from tutorial.items import DmozItem
+
 
 class DmozSpider(scrapy.Spider):
 	name = 'dmoz'
@@ -6,7 +8,16 @@ class DmozSpider(scrapy.Spider):
 		"http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
 			]
+
 	def parse(self, response):
-		filename = response.url.split('/')[-2] + '.html'
-		with open(filename, 'wb') as f:
-			f.write(response.body)
+		for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+			url = response.urljoin(href.extract())
+			yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+	def parse_dir_contents(self, response):
+		for sel in response.xpath('//ul/li'):
+			item = DmozItem()
+			item['title'] = sel.xpath('a/text()').extract()
+			item['link']  = sel.xpath('a/@href').extract()
+			item['desc']  = sel.xpath('text()').extract()
+			yield item
